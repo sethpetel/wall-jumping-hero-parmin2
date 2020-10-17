a2af1bfa3b9bc7480fcdd4b2a250032711bad238
sethpetel <44174039+sethpetel@users.noreply.github.com>
2020-10-17 14:50:19 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,72 @@
+let rightSwordOutImg = img`
+    . . . . . . . f f . . . . . . . 
+    . . . . f f f f 2 f f . . . . . 
+    . . f f e e e e f 2 f f . . . . 
+    . f f e e e e e f 2 2 f f . . . 
+    . f e e e e f f e e e e f . . . 
+    . f f f f f e e 2 2 2 2 e f . . 
+    f f f e 2 2 2 f f f f e 2 f . . 
+    f f f f f f f f e e e f f f . . 
+    f e f e 4 4 e b f 4 4 e e f . . 
+    . f e e 4 d 4 b f d d e f . . . 
+    . . f e e e 4 d d d e e . c . . 
+    . . . f 2 2 2 2 e e d d e c c c 
+    . . . f 4 4 4 e 4 4 d d e c d d 
+    . . . f f f f f e e e e . c c c 
+    . . f f f f f f f f . . . c . . 
+    . . f f f . . f f . . . . . . . 
+    `
+let leftSwordOutImg = img`
+    . . . . . . . f f . . . . . . . 
+    . . . . . f f 2 f f f f . . . . 
+    . . . . f f 2 f e e e e f f . . 
+    . . . f f 2 2 f e e e e e f f . 
+    . . . f e e e e f f e e e e f . 
+    . . f e 2 2 2 2 e e f f f f f . 
+    . . f 2 e f f f f 2 2 2 e f f f 
+    . . f f f e e e f f f f f f f f 
+    . . f e e 4 4 f b e 4 4 e f e f 
+    . . . f e d d f b 4 d 4 e e f . 
+    . . c . e e d d d 4 e e e f . . 
+    c c c e d d e e 2 2 2 2 f . . . 
+    d d c e d d 4 4 e 4 4 4 f . . . 
+    c c c . e e e e f f f f f . . . 
+    . . c . . . f f f f f f f f . . 
+    . . . . . . . f f . . f f f . . 
+    `
+let rightFacingImg = img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . f f f f f f . . . . . 
+    . . . f f e e e e f 2 f . . . . 
+    . . f f e e e e f 2 2 2 f . . . 
+    . . f e e e f f e e e e f . . . 
+    . . f f f f e e 2 2 2 2 e f . . 
+    . . f e 2 2 2 f f f f e 2 f . . 
+    . f f f f f f f e e e f f f . . 
+    . f f e 4 4 e b f 4 4 e e f . . 
+    . f e e 4 d 4 1 f d d e f . . . 
+    . . f e e e e e d d d f . . . . 
+    . . . . f 4 d d e 4 e f . . . . 
+    . . . . f e d d e 2 2 f . . . . 
+    . . . f f f e e f 5 5 f f . . . 
+    . . . f f f f f f f f f f . . . 
+    . . . . f f . . . f f f . . . . 
+    `
+let leftFacingImg = img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . f f f f f f . . . . . 
+    . . . . f 2 f e e e e f f . . . 
+    . . . f 2 2 2 f e e e e f f . . 
+    . . . f e e e e f f e e e f . . 
+    . . f e 2 2 2 2 e e f f f f . . 
+    . . f 2 e f f f f 2 2 2 e f . . 
+    . . f f f e e e f f f f f f f . 
+    . . f e e 4 4 f b e 4 4 e f f . 
+    . . . f e d d f 1 4 d 4 e e f . 
+    . . . . f d d d e e e e e f . . 
+    . . . . f e 4 e d d 4 f . . . . 
+    . . . . f 2 2 e d d e f . . . . 
+    . . . f f 5 5 f e e f f f . . . 
+    . . . f f f f f f f f f f . . . 
+    . . . . f f f . . . f f . . . . 
+    `
